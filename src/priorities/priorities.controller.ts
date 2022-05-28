8455ed33efce5f923983bf61a2206f28208c5a0d
Kemps Medeiros <kemps.medeiros@kabum.com.br>
2022-05-28 10:17:34 -0300

    created personas and descriptions, cuthenticated  priorities routes

--- src/priorities/priorities.controller.ts
+++ src/priorities/priorities.controller.ts
@@ -1,32 +1,40 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe, Put } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe, Put, UseGuards } from '@nestjs/common';
 import { PrioritiesService } from './priorities.service';
 import { CreatePriorityDto } from './dto/create-priority.dto';
 import { UpdatePriorityDto } from './dto/update-priority.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('app/api/priorities')
 export class PrioritiesController {
   constructor(private readonly prioritiesService: PrioritiesService) {}
 
+
+  @UseGuards(JwtAuthGuard)  
   @Post()
   async create(@Body() createPriorityDto: CreatePriorityDto) {
     return await this.prioritiesService.create(createPriorityDto);
   }
-
+  
+  
+  @UseGuards(JwtAuthGuard)  
   @Get()
   async findAll() {
     return await this.prioritiesService.findAll();
   }
-
+  
+  @UseGuards(JwtAuthGuard)  
   @Get(':id')
   async findOne(@Param('id', new ParseUUIDPipe()) id: string) {
     return await this.prioritiesService.findOneOrFail({id});
   }
-
+  
+  @UseGuards(JwtAuthGuard)  
   @Put(':id')
   async update(@Param('id', new ParseUUIDPipe()) id: string, @Body() updatePriorityDto: UpdatePriorityDto) {
     return await this.prioritiesService.update(id, updatePriorityDto);
   }
-
+  
+  @UseGuards(JwtAuthGuard)  
   @Delete(':id')
   async remove(@Param('id', new ParseUUIDPipe()) id: string) {
     return await this.prioritiesService.remove(id);
